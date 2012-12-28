4a3824b816b1d60c6d1284e6580f187fdea74997
Jean-Baptiste Delhommeau <jbdelhommeau@jbd-desktop.(none)>
2012-12-28 15:33:05 +0100

    new file:   index.html 	new file:   libs/app.js 	new file:   libs/bootstrap/css/bootstrap-responsive.css 	new file:   libs/bootstrap/css/bootstrap-responsive.min.css 	new file:   libs/bootstrap/css/bootstrap.css 	new file:   libs/bootstrap/css/bootstrap.min.css 	new file:   libs/bootstrap/img/glyphicons-halflings-white.png 	new file:   libs/bootstrap/img/glyphicons-halflings.png 	new file:   libs/bootstrap/js/bootstrap.js 	new file:   libs/bootstrap/js/bootstrap.min.js 	new file:   libs/picrop.css 	new file:   libs/picrop.jquery.js

---
+++ libs/app.js
@@ -0,0 +1,5 @@
+$(function(){
+
+		$(".picrop").Picrop();
+
+});
---
+++ libs/picrop.jquery.js
@@ -0,0 +1,107 @@
+/*!
+* jQuery Picrop - Version: 20121227
+* http://www.google.fr
+* Copyright (c) 2012 JB. Delhommeau; Dual licensed: MIT/GPL
+* Requires: jQuery v1.7 or later
+*/
+
+(function($){
+	
+	$.fn.Picrop = function(method){
+
+		if ( methods[method] ) {
+			return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.init.apply( this, arguments );
+		} else {
+			$.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
+		}
+
+	};
+
+	//Variables list
+	var settings,
+		els;
+
+	var methods = {
+		init : function( options ){
+			settings = $.extend( {
+				'fx'		: false,
+				'duration'	: 200,
+				'loader'	: 'loading'
+			}, options);
+
+			els = $(this),
+			pics = els.find("img");
+
+			methods.startHandle();
+
+		},
+
+		startHandle : function(){
+			if(pics.length > 0){
+				pics.not(".croped").hide();
+				els.addClass(settings.loader);
+
+				$(pics).each(function(){
+					if(this.naturalWidth === 0){
+						var src = $(this).data('src'),
+							fx = $(this).data('fx');
+						if(fx === undefined){
+							fx = settings.fx;
+						}
+						if(!src){
+							src = $(this).attr('src');
+						}
+						methods.loadPictures(src, fx, this);
+					}else{
+						methods.crop(this, settings.fx);
+					}
+				});
+			}
+		},
+
+		loadPictures: function(src, fx, original){
+			original.onload = function(){
+				methods.crop($(this), fx);
+			};
+			original.src = src;
+		},
+
+		crop: function(pic, fx){
+
+			var wrappic = pic.parent(), 
+				wpic = pic.width(),
+				hpic = pic.height(),
+				wwpic = wrappic.width(),
+				hhpic = wrappic.height(),
+				hwpic = ((hpic * wwpic) / wpic),
+				whpic = ((wpic * hhpic) / hpic);
+			var duration = pic.data('duration');
+
+			if(hwpic >= hhpic){
+				var rectify = ((hwpic - hhpic) / 2);
+				pic.width(wwpic)
+				   .height(hwpic)
+				   .css("margin-top", "-" + rectify + "px");
+			}else if(whpic >= wwpic){
+				var rectify = ((whpic - wwpic) / 2);
+				pic.height(hhpic)
+				   .width(whpic)
+				   .css("margin-left", "-" + rectify + "px");
+			}
+
+			if(duration === undefined || isNaN(parseInt(duration))){
+				duration = settings.duration;
+			}
+
+			wrappic.removeClass(settings.loader);
+			if(fx !== false){
+				pic.addClass("croped").fadeIn(duration);
+			}else{
+				pic.addClass("croped").show();
+			}
+		}
+	};
+
+})(jQuery);
